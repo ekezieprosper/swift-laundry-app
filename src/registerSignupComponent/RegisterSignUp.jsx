38a14b35fc2ce2daf352e175fcb1c1a1e11e258e
ekezieprosper <japhethprosper234@gmail.com>
2024-03-07 10:51:22 +0100

    ggggg

--- src/registerSignupComponent/RegisterSignUp.jsx
+++ src/registerSignupComponent/RegisterSignUp.jsx
@@ -78,7 +78,7 @@ const RegisterSignUp = () => {
         text: response.data?.message,
         icon: "success"
       });
-      navigate("/registerLogin")
+      navigate("/businessDashboard")
       localStorage.setItem('admindata', JSON.stringify(response.data?.data))
 
     } catch (err) {
